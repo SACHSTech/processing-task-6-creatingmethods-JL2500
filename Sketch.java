97dc5ec7d20f94bce2b41f1219a75a3279fea9b4
Julian3377 <julianli3377@gmail.com>
2023-11-09 11:03:16 -0500

    Added javadoc comments

--- Sketch.java
+++ Sketch.java
@@ -14,6 +14,7 @@ public class Sketch extends PApplet {
 
   public void draw() {
 
+    // Draws a pattern of smiley faces around the screen
     for (int i = 50; i < width; i += 100) {
       for (int j = 50; j < height; j += 100) {
         int smileyColour = faceColour(i, j);
@@ -21,14 +22,24 @@ public class Sketch extends PApplet {
       }
     }
 
+    // Draws a pattern of houses in the middle of the smiley faces
     for (int k = 50; k < width - 50; k += 100) {
       for (int l = 50; l < height - 50; l += 100) {
-        drawHouse(k + 40, l + 40, 25, 25);
+        drawHouse(k + 40, l + 40, 25);
       }
     }
 
   }
   
+  /**
+  * Draws a smiley face given the parameters
+  *
+  * @param x  The x coordinate of where the face is drawn
+  * @param y  The y coordinate of where the face is drawn
+  * @param diameter  How big the smiley face is
+  * @param colour  The stroke colour of the face
+  *
+  */  
   void drawSmileyFace(float x, float y, float diameter, int colour) {
     noFill();
     stroke(colour);
@@ -46,12 +57,28 @@ public class Sketch extends PApplet {
     arc(x, y, smileSize, smileSize, radians(0), radians(180));
   }
   
-  void drawHouse(float houseX, float houseY, float houseWidth, float houseHeight) {
+  /**
+  * Draws a house given the parameters
+  *
+  * @param houseX  The x coordinate of where the house is drawn
+  * @param houseY  The y coordinate of where the house is drawn
+  * @param houseWidth  How wide the house is
+  *
+  */  
+  void drawHouse(float houseX, float houseY, float houseWidth) {
     fill(255, 204, 0);
-    rect(houseX, houseY, houseWidth, houseHeight);
-    triangle(houseX, houseY, houseX + houseWidth / 2, houseY - houseHeight / 2, houseX + houseWidth, houseY);
+    rect(houseX, houseY, houseWidth, houseWidth);
+    triangle(houseX, houseY, houseX + houseWidth / 2, houseY - houseWidth / 2, houseX + houseWidth, houseY);
   }
 
+  /**
+  * Given x and y of drawing, returns a colour based on its location
+  *
+  * @param x  The x coordinate of where the drawing is
+  * @param y  The y coordinate of where the drawing is
+  * @return Returns red, black, or blue, depending on location
+  *
+  */  
   int faceColour(int x, int y) {
     float section = map(x, 0, width, 0, 3);
 
